1d6a75fb6305db3c976b16bd01c7830ce96ed347
Hans5958 <email5958.1@gmail.com>
2021-04-01 12:18:12 +0700

    Improve libraries check

--- src/libraries.js
+++ src/libraries.js
@@ -2,17 +2,18 @@
 
 module.exports = () => {
 
-	const pr = danger.github.pr;
 	const modified = danger.git.modified_files;
 
 	const modifiedLibFiles = modified.filter((p) => p.includes("libraries/"));
 
-	if (modifiedLibFiles.includes("libraries/")) {
+	if (modifiedLibFiles !== 0) {
 		if (
 			!modifiedLibFiles.includes("libraries/CREDITS.md") ||
 			!modifiedLibFiles.includes("libraries/license-info.json")
 		) {
-			warn("There are library changes, but you didn't change CREDITS.md or license-info.json. Do they need changing?");
+			warn("There are library changes, but CREDITS.md or license-info.json is left unchanged. Confirm if they need changing.");
+			markdown("## Libraries changed")
+			markdown(modifiedLibFiles.map(file => `- ${file}`).join("\n"))
 		}
 	}
 	
